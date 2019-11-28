af0d3d331d588c0cfbf7c42f0c60724d551236ef
CHARRUEL Maxime <Maxime.CHARRUEL-ext@ca-ts.fr>
2019-11-28 16:53:36 +0100

    Initial Commit

---
+++ ReduxSeries/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ReduxSeries
+//
+//  Created by CHARRUEL Maxime on 28/11/2019.
+//  Copyright © 2019 CHARRUEL Maxime. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
